546b2ee0e06e5467b9653a974337a6664f80788e
nadiaadila <nadiaadilabtmuhammad@gmail.com>
2016-10-24 11:28:48 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(lubridate)
+setwd("C:/Users/saya/Desktop/DS/real kuiz/Exploratory Data Analysis/Week 1")
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+png("plot1.png", width=480, height=480)
+hist(data$V3, col = "red", main = "Global Active Power",xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+dev.off()
